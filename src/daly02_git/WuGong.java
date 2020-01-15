91d830e9c2c6bfd3a511ac7208d1e179559929c4
shifu <mst1142945451@qq.com>
2020-01-15 08:36:53 +0800

    shifu:xxxxxx

--- src/daly02_git/WuGong.java
+++ src/daly02_git/WuGong.java
@@ -5,6 +5,7 @@ public class WuGong {
 	public static void main(String[] args) {
 		System.out.println("wugong");
 		System.out.println("tudu:从hub上clone并编辑了一行");
+		System.out.println("冲突：xxxxxxxxxxx");
 	}
 
 }
